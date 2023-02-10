dee3bccdbdc19bdd12279b5b93a8e10cafad30a2
Ваше Имя <you@example.com>
2023-02-10 16:01:12 +0300

    Add random mines

--- Minesweeper/sfml-app.cpp
+++ Minesweeper/sfml-app.cpp
@@ -1,10 +1,63 @@
+//2:08 draw field
+//4:40 random mines
+
 #include <SFML/Graphics.hpp>
+#include <time.h>
 
 using namespace sf;
 
 int main()
 {
+    srand(time(0));
+
     RenderWindow window(VideoMode(400, 400), "Minesweeper");
+
+    int w = 32;
+    int grid[12][12];
+    int sgrid[12][12];
+    
+    Texture t;
+    t.loadFromFile("Paint/tiles.png");
+    Sprite tiles(t);
+
+    for(int i = 1; i <= 10; i++)
+        for(int j = 1; j <= 10; j++)
+            {
+            sgrid[i][j] = 10;
+            
+            if(rand() % 5 == 0)
+                grid[i][j] = 9;
+            else
+                grid[i][j] = 0;
+
+            }
+    
+    for(int i = 1; i <= 10; i++)
+        for(int j = 1; j <= 10; j++)
+        {
+            int n = 0;
+
+            if(grid[i][j] == 9)
+                continue;
+            if(grid[i + 1][j] == 9)
+                n++;
+            if(grid[i][j + 1] == 9)
+                n++;
+            if(grid[i - 1][j] == 9)
+                n++;
+            if(grid[i][j - 1] == 9)
+                n++;
+            if(grid[i + 1][j + 1] == 9)
+                n++;
+            if(grid[i - 1][j - 1] == 9)
+                n++;
+            if(grid[i - 1][j + 1] == 9)
+                n++;
+            if(grid[i + 1][j - 1] == 9)
+                n++;
+
+            grid[i][j] = n;
+        }
 
     while(window.isOpen())
     {
@@ -16,6 +69,17 @@ int main()
         }
 
         window.clear(Color::White);
+
+        for(int i = 1; i <= 10; i++)
+            for(int j = 1; j <= 10; j++)
+            {
+                sgrid[i][j] = grid[i][j];
+                tiles.setTextureRect(IntRect(sgrid[i][j] * w,  0, w, w));
+                tiles.setPosition(i * w, j * w);
+
+                window.draw(tiles);
+            }
+
         window.display();
     }
 
