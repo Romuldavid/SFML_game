bd76adc513cfda1e274e22d6fff04f271580e70c
Ваше Имя <you@example.com>
2023-02-15 11:25:44 +0300

    Add rotation pipe

--- water_sup/sfml-app.cpp
+++ water_sup/sfml-app.cpp
@@ -1,3 +1,6 @@
+//1:09 add pipe
+//2:23 add rotation pipe
+
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
@@ -5,6 +8,13 @@ using namespace sf;
 const int M = 7, N = 5;
 int ts = 80;
 Vector2f offset(65, 55);
+
+struct Pipe{
+    int orientation;
+    float angle;
+
+    Pipe() { angle = 0;}
+} grid[M][N];
 
 int main()
 {
@@ -22,6 +32,15 @@ int main()
         {
             if(event.type == Event::Closed)
                 window.close();
+
+            if(event.type == Event::MouseButtonPressed)
+                if(event.key.code == Mouse::Left) {
+                    Vector2i pos = Mouse::getPosition(window) + 
+                        Vector2i(ts / 2, ts / 2) - Vector2i(offset);
+
+                    pos /= ts;
+                    grid[pos.x][pos.y].orientation++;
+                }
         }
 
         window.clear(Color::White);
@@ -29,6 +48,15 @@ int main()
         for(int i = 0; i < N; i++)
             for(int j  = 0; j < M; j++)
                 {
+
+                Pipe& pe = grid[j][i];
+
+                pe.angle += 5;
+                if(pe.angle > pe.orientation * 90)
+                    pe.angle  =  pe.orientation * 90;
+
+                pipe.setRotation(pe.angle);
+
                 pipe.setTextureRect(IntRect(ts * 2, 0, ts,  ts));
 
                 pipe.setPosition(j * ts, i * ts);
